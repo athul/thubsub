debbef54bd932e7f98cbcb2884e2fa38034caaa2
Divya Ann Kurien <divya.kurien@gmail.com>
2020-03-17 15:34:38 +0530

    Changed

--- src/acronym.c
+++ src/acronym.c
@@ -1,1 +1,26 @@
 #include "acronym.h"
+#include "../src/acronym.h"
+#include <stdlib.h>
+#include <string.h>
+#include<ctype.h>
+	
+char *abbreviate(const char *phrase)
+{
+  	if(phrase==NULL) 
+  		return NULL;
+ 	if(strcmp(phrase,"")==0) 
+    	return NULL;
+	char *s;
+	s = (char *)malloc(sizeof(char)*100);
+    int i=0,j=0,n;
+    n=strlen(phrase);
+    s[j++] = toupper(phrase[i]);
+    
+    for(i=1;i<n;i++)
+    {
+       if(phrase[i]==' '||phrase[i]=='-')
+       		s[j++]=toupper(phrase[i+1]);
+    }
+    s[j]='\0';
+    return s;	
+}
