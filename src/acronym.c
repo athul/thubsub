7c965b756a376ab09c1808b9e61c38d9cf043e59
Aswin Gopakumar <aswing005@gmail.com>
2020-03-17 23:37:49 +0530

    Update acronym.c

--- src/acronym.c
+++ src/acronym.c
@@ -1,1 +1,31 @@
 #include "acronym.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+void  main(){
+char ip[1000];
+char op[1000];
+int n=50;
+int i=0,j=1;
+scanf("%[^\n]",ip);
+while(ip[i]!='\0'){
+	if(ip[i]>='a' && ip[i]<='z')
+ 	{
+	ip[i]-=32;
+	}
+	i++;
+}
+op[0]=ip[0];
+//printf("%c",ip[0]);
+i=0;
+while(ip[i]!='\0'){
+if(ip[i]==' ')
+op[j++]=ip[++i];
+i++;
+}
+op[j]='\0';
+printf("%s",op);
+//int length=strlen(ip);
+//for(i=0;i<length;i++){
+}
