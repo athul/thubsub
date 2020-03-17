433e0f51591257a8737ecb054e95c45e94a63733
Ajay Prakash Nair <42165927+ajaynair710@users.noreply.github.com>
2020-03-17 15:09:30 +0530

    update

--- src/acronym.c
+++ src/acronym.c
@@ -1,26 +1,26 @@
 #include "acronym.h"
-#include<stdlib.h>
-#include<stdio.h>
-#include<string.h>
-#include<ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-char *abbreviate(const char *phrase){
-    if(!phrase) return NULL;
-    if(*phrase == '\0')return NULL;
-    char *acr;
-    acr = (char *)malloc(sizeof(char)*100);
-    int k=1,len,i;
-    len=strlen(phrase);
-    
-    acr[0]=toupper(phrase[0]);
-    for(i=1;i<len;i++)
-    if(phrase[i]==' '||phrase[i]=='-')
+char *abbreviate(const char *phrase)
+{
+ char *acronym=NULL;
+ const char *delim = " -_";
+ int i = 0;
+ char *next = NULL;
+
+ if((phrase)&&(*phrase!='\0'))
+ {
+   char phrasein[MAX_SIZE];
+   acronym = (char *)malloc(sizeof(char) * MAX_SIZE);
+   strncpy(phrasein, phrase, MAX_SIZE);
+   next = strtok(phrasein, delim);
+   while(next != NULL)
     {
-    acr[k]=toupper(phrase[i+1]);
-    k++;
+      acronym[i++] = toupper(*next);
+      next = strtok(NULL, delim);
     }
-
-    acr[k]='\0';
-    return acr;
-
+ }
+ return acronym;
 }
--- src/acronym.h
+++ src/acronym.h
@@ -1,6 +1,7 @@
 #ifndef ACRONYM_H
 #define ACRONYM_H
 
+#define MAX_SIZE 100
 char *abbreviate(const char *phrase);
 
 #endif
