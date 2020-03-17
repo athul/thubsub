5498504f3ae9acc44a6cd3e9a9b54d1fa2347485
accakks <aakanksha.jain8@gmail.com>
2020-03-17 21:20:48 +0530

    C program added, checks passed

--- src/acronym.c
+++ src/acronym.c
@@ -1,1 +1,20 @@
 #include "acronym.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+char *abbreviate(const char *phrase)
+   {
+      if((phrase == NULL) || phrase[0] == '\0')
+            return NULL;
+       char *acronym;
+       acronym = (char *)malloc(sizeof(char)*strlen(phrase));
+       acronym[0] = toupper(phrase[0]);
+       unsigned int j = 1;
+       for(unsigned int i=1;i<strlen(phrase);i++)
+          if(((phrase[i]==' ')||(phrase[i]=='-'))&&(phrase[i+1]!=' ')&&(phrase[i+1]!='\0'))
+            {acronym[j] = toupper(phrase[i+1]);
+               j+=1;}
+       return acronym;
+   } 
