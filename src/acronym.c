728955a4c7908986652718c1866efb338612e53d
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2020-03-17 19:20:27 +0530

    Almost done

--- src/acronym.c
+++ src/acronym.c
@@ -1,16 +1,26 @@
 #include "acronym.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 char *abbreviate(const char *phrase)
    {
-       phrase=toupper(phrase);
-       printf("%s",phrase);
-       char* accr= "";
-       for(int i=0;i<strlen(phrase)-1;i++)
+       int i=0,j=0;
+       char *accr= "";
+       accr = (char *)malloc(sizeof(char)*strlen(phrase));
+       accr[j] = toupper(phrase[0]);
+       j++;
+       for(i=1;i<strlen(phrase)-1;i++)
           {
-              
+              if((phrase[i]==' ')&&(phrase[i+1]!=' ')&&(phrase[i+1]!='\0'))
+                 {
+                     accr[j] = toupper(phrase[i+1]);
+                 }
           }
+       j++;
+       accr[j]='\0'; 
+       printf("%s",accr);
        return 0;
    }
 
