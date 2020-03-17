36081cd8046d926946c30f7b4166d17598b5c99d
Vishnu <vishnunarayanan@accubits.com>
2020-03-17 23:13:06 +0530

    added acronym code

--- src/acronym.c
+++ src/acronym.c
@@ -1,1 +1,22 @@
 #include "acronym.h"
+#include<string.h>
+#include<stdio.h>
+void main()
+{
+int k=1,i;
+char st[100],st2[10];
+
+printf("\n\nEnter The String : ");
+gets(st);
+st2[0]=toupper(st[0]);
+for(i=1;i<strlen(st);i++)
+    if(st[i]==' ')
+    {
+    st2[k]=toupper(st[i+1]);
+    k++;
+    }
+printf("\n\nThe ACRONYM Is : ");
+for(i=0;i<k;i++)
+    printf("%c",st2[i]);
+ return 0;
+}
