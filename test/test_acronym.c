cd09df31f2fe6ee016a36c39931a83f6e4dc4c99
Divya Ann Kurien <divya.kurien@gmail.com>
2020-03-17 14:03:25 +0530

    All cases passed

--- test/test_acronym.c
+++ test/test_acronym.c
@@ -2,13 +2,27 @@
 #include "../src/acronym.h"
 #include <stdlib.h>
 #include <string.h>
-
-void setUp(void)
+#include<ctype.h>
+	
+char *abbreviate(const char *phrase)
 {
-}
-
-void tearDown(void)
-{
+  	if(phrase==NULL) 
+  		return NULL;
+ 	if(strcmp(phrase,"")==0) 
+    	return NULL;
+	char *s;
+	s = (char *)malloc(sizeof(char)*100);
+    int i=0,j=0,n;
+    n=strlen(phrase);
+    s[j++] = toupper(phrase[i]);
+    
+    for(i=1;i<n;i++)
+    {
+       if(phrase[i]==' '||phrase[i]=='-')
+       		s[j++]=toupper(phrase[i+1]);
+    }
+    s[j]='\0';
+    return s;	
 }
 
 static void test_abbreviation(char *phrase, char *expected)
