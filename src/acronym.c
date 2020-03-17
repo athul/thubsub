b16164ca4ebcdbb6d7869583608cb9bef0176df3
nandakishormpai2001 <57388834+nandakishormpai2001@users.noreply.github.com>
2020-03-17 11:50:34 +0530

    update

--- src/acronym.c
+++ src/acronym.c
@@ -1,18 +1,10 @@
-#ifndef ACRONYM_H
-#define ACRONYM_H
-
-char *abbreviate(const char *phrase);
-
-#endif
-
-
 #include <stdio.h>
 int main()
 {
     int i,j;
     char name[100],acr[10];
 	printf("\nEnter The long name\n");
-	scanf("%[^\n]",name);
+	scanf("%[^\n]",&name);
 	acr[0]=name[0];
 	j=1;
 	for(i=1;name[i+1]!='\0';++i)
