2e1ba873843ccf85b9fedd89ed6bc1d1bc750f47
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2020-03-17 18:21:55 +0530

    Env Set

--- src/acronym.c
+++ src/acronym.c
@@ -1,1 +1,21 @@
 #include "acronym.h"
+#include <stdio.h>
+#include <string.h>
+
+char *abbreviate(const char *phrase)
+   {
+       phrase=toupper(phrase);
+       printf("%s",phrase);
+       char* accr= "";
+       for(int i=0;i<strlen(phrase)-1;i++)
+          {
+              
+          }
+       return 0;
+   }
+
+
+   void main()
+     {
+         abbreviate("Chrisbin Sunny");
+     }
