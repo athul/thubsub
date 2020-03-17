41d573876134e0b89c7efd7c6748b91062614a9c
Hari Govind <hgs@disroot.org>
2020-03-17 12:00:16 +0530

    acronyms always caps

--- src/acronym.c
+++ src/acronym.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 char *abbreviate(const char *phrase) {
 	if(phrase == NULL) {
@@ -11,11 +12,11 @@ char *abbreviate(const char *phrase) {
 	abbr = (char *)malloc(sizeof(char)*100);
 	int len = 0, j=0;
 	len = strlen(phrase);
-	abbr[j] = phrase[0];
+	abbr[j] = toupper(phrase[0]);
 	for(int i=0; i<len; i++){
 		if(phrase[i] == ' '){
 			j++;
-			abbr[j] = phrase[i+1];
+			abbr[j] = toupper(phrase[i+1]);
 		}
 	}
 	return abbr;
