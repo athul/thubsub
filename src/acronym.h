66a596b201968e1a75a9daf41652f012f3d0c603
Ash_win <62272001+ash-win-cs@users.noreply.github.com>
2020-03-18 00:46:47 +0530

    Code updated

--- src/acronym.h
+++ src/acronym.h
@@ -1,6 +1,22 @@
 #ifndef ACRONYM_H
 #define ACRONYM_H
 
-char *abbreviate(const char *phrase);
+const char *abbreviat(const char* phrase)
+{
+ 
+  int c = 1;
+  static char s[20];
+  s[0] = toupper(phrase[0]);
+  
+  
+  for(int i= 0; phrase[i] != '\0'; i++){
+    if(phrase[i] == ' ' || phrase[i] == '-'){
+      s[c] = toupper(phrase[i+1]);
+      c++;
+    }
+  } 
+  return s;
+}
+
 
 #endif
