5801d02b899b12c0ef685a74a8fd16adb03b9221
Vishnu <vishnunarayanan@accubits.com>
2020-03-18 00:22:13 +0530

    removed warings and updates

--- src/acronym.c
+++ src/acronym.c
@@ -1,18 +1,20 @@
-#include "acronym.h"
+// #include "acronym.h"
 #include<string.h>
 #include<stdio.h>
-void main()
+int main()
 {
 int k=1,i;
 char st[100],st2[10];
 
 printf("\n\nEnter The String : ");
-gets(st);
-st2[0]=toupper(st[0]);
+fgets(st, sizeof(st), stdin);
+
+st2[0]=st[0];
 for(i=1;i<strlen(st);i++)
     if(st[i]==' ')
     {
-    st2[k]=toupper(st[i+1]);
+    
+    st[k]=st[i+1];
     k++;
     }
 printf("\n\nThe ACRONYM Is : ");
