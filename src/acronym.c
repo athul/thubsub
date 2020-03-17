a1c2fe87d414b6951d5e3576acc6c62911356a67
MeenakshiShankar <me30shankar@gmail.com>
2020-03-17 16:46:30 +0530

    code entered

--- src/acronym.c
+++ src/acronym.c
@@ -1,1 +1,21 @@
 #include "acronym.h"
+
+void main()
+{
+int k=1,i;
+char st[100],st2[10];
+clrscr();
+printf("\n\nEnter The String : ");
+gets(st);
+st2[0]=toupper(st[0]);
+for(i=1;i<strlen(st);i++)
+    if(st[i]==' ')
+    {
+    st2[k]=toupper(st[i+1]);
+    k++;
+    }
+printf("\n\nThe ACRONYM Is : ");
+for(i=0;i<k;i++)
+    printf("%c",st2[i]);
+getch();
+}
--- src/acronym.h
+++ src/acronym.h
@@ -1,6 +1,7 @@
 #ifndef ACRONYM_H
 #define ACRONYM_H
 
+
 char *abbreviate(const char *phrase);
 
 #endif
