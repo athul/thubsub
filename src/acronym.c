2d8426c0f1bf627fbed09541a5009bce6eebcdfc
nandakishormpai2001 <57388834+nandakishormpai2001@users.noreply.github.com>
2020-03-17 10:43:45 +0530

    Update acronym.c
    I have added the code for generating Acronyms of Long names inputted

--- src/acronym.c
+++ src/acronym.c
@@ -1,1 +1,21 @@
-#include "acronym.h"
+#include <stdio.h>
+int main()
+{
+    int i,j;
+    char name[100],acr[10];
+	printf("\nEnter The long name\n");
+	scanf("%[^\n]",&name);
+	acr[0]=name[0];
+	j=1;
+	for(i=1;name[i+1]!='\0';++i)
+	{
+	    if(name[i]==' ')
+	    {
+	        acr[j]=name[i+1];
+	        ++j;
+	    }
+	}
+	acr[j]='\0';
+	printf("\nAcronym of %s is %s",name,acr);
+    return 0;
+}
