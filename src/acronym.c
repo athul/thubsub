0ccc688ac2795e202869d57479be281ed462627c
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2020-03-17 19:37:47 +0530

    Done

--- src/acronym.c
+++ src/acronym.c
@@ -7,25 +7,20 @@
 char *abbreviate(const char *phrase)
    {
        int i=0,j=0;
+
+       if((phrase == NULL) || phrase[0] == '\0')
+            return NULL;
+
        char *accr= "";
        accr = (char *)malloc(sizeof(char)*strlen(phrase));
        accr[j] = toupper(phrase[0]);
        j++;
        for(i=1;i<strlen(phrase)-1;i++)
-          {
-              if((phrase[i]==' ')&&(phrase[i+1]!=' ')&&(phrase[i+1]!='\0'))
-                 {
-                     accr[j] = toupper(phrase[i+1]);
-                 }
-          }
-       j++;
-       accr[j]='\0'; 
-       printf("%s",accr);
-       return 0;
+          if(((phrase[i]==' ')||(phrase[i]=='-'))&&(phrase[i+1]!=' ')&&(phrase[i+1]!='\0'))
+            {
+               accr[j] = toupper(phrase[i+1]);
+               j++;
+             }
+       accr[j]='\0';
+       return accr;
    }
-
-
-   void main()
-     {
-         abbreviate("Chrisbin Sunny");
-     }
