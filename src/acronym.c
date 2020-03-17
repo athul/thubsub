ac7ca019b9c73f735dde6b49fbb87a29a4a8d075
kikku <60809011+pckikku@users.noreply.github.com>
2020-03-17 16:57:15 +0530

    Update acronym.c

--- src/acronym.c
+++ src/acronym.c
@@ -1,1 +1,26 @@
 #include "acronym.h"
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+void main()
+{
+
+    int j=1,i;
+    char word[100],acro[20];
+    printf("Enter The Contents\n");
+    gets(word);
+    acro[0]=toupper(word[0]);
+    for(i=1;i<strlen(word);i++)
+    {
+        if(word[i]==' ')
+        {
+
+            acro[j]=toupper(word[i+1]);+
+            j++;
+
+        }
+    }
+acro[j]='\0';
+    printf("The Acronym of %s is %s",word,acro);
+getch();
+}
