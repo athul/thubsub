f93b5b46cd7a90cc260165a3c6ca1a1933228799
Hari Govind <hgs@disroot.org>
2020-03-17 11:55:42 +0530

    test_basic_abbreviation:PASS

--- src/acronym.c
+++ src/acronym.c
@@ -1,1 +1,19 @@
 #include "acronym.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+char *abbreviate(const char *phrase) {
+	char *abbr;
+	abbr = (char *)malloc(sizeof(char)*100);
+	int len = 0, j=0;
+	len = strlen(phrase);
+	abbr[j] = phrase[0];
+	for(int i=0; i<len; i++){
+		if(phrase[i] == ' '){
+			j++;
+			abbr[j] = phrase[i+1];
+		}
+	}
+	return abbr;
+}
