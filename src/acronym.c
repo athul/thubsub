dea42721002fe6f00fdc0fea0ccc8f0446385cc7
johns-thomas <johns18111998@gmail.com>
2020-03-17 22:19:32 +0530

    Coded

--- src/acronym.c
+++ src/acronym.c
@@ -1,1 +1,32 @@
 #include "acronym.h"
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+
+char *abbreviate(const char *phrase){
+	
+	if(phrase!=NULL && phrase[0]!='\0'){
+	
+	
+	char *tlp = (char *)malloc(sizeof(char)*50);
+	int j=1;
+	
+	*tlp=toupper(phrase[0]);
+	for (++phrase; *phrase; phrase++) {
+
+		if(*phrase==' '||*phrase=='-'){
+			phrase++;
+			*(tlp+j)=toupper(*phrase);
+			j++;
+		}
+	}
+	
+	*(tlp+j)='\0';
+	return tlp;
+	}else{
+		return NULL;
+	}
+   }
+		
+
+
